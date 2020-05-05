0ba9ef8aed59943b1667e8b156f1f630205d419e
Pieter Sartain <pesartain@googlemail.com>
2020-05-05 16:53:28 +0100

    Create ConnectionExtensions

---
+++ app/src/main/java/com/example/shiori/ConnectionExtensions.kt
@@ -0,0 +1,43 @@
+package com.example.shiori
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.collection.LruCache
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.ImageLoader
+import com.android.volley.toolbox.Volley
+import java.sql.Connection
+
+class ConnectionExtensions constructor(context: Context) {
+    companion object {
+        @Volatile
+        private var INSTANCE: ConnectionExtensions? = null
+        fun getInstance(context: Context) =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: ConnectionExtensions(context).also {
+                    INSTANCE = it
+                }
+            }
+    }
+    val imageLoader: ImageLoader by lazy {
+        ImageLoader(requestQueue,
+            object : ImageLoader.ImageCache {
+                private val cache = LruCache<String, Bitmap>(20)
+                override fun getBitmap(url: String): Bitmap? {
+                    return cache.get(url)
+                }
+                override fun putBitmap(url: String, bitmap: Bitmap) {
+                    cache.put(url, bitmap)
+                }
+            })
+    }
+    val requestQueue: RequestQueue by lazy {
+        // applicationContext is key, it keeps you from leaking the
+        // Activity or BroadcastReceiver if someone passes one in.
+        Volley.newRequestQueue(context.applicationContext)
+    }
+    fun <T> addToRequestQueue(req: Request<T>) {
+        requestQueue.add(req)
+    }
+}
