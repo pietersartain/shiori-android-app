f5f572d08bf17d118a7dac3bbed5d46c069e2b55
Pieter Sartain <pesartain@googlemail.com>
2020-05-24 02:04:29 +0100

    Adds article list content

--- app/src/main/java/com/example/shiori/ArticleAdapter.kt
+++ app/src/main/java/com/example/shiori/ArticleAdapter.kt
@@ -4,12 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.recycler_article_list_item.view.*
 import org.json.JSONArray
 
 class ArticleAdapter(val items : JSONArray, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+
+    private val prefs = PreferenceManager.getDefaultSharedPreferences(context)
 
     // Gets the number of articles in the list
     override fun getItemCount(): Int {
@@ -21,13 +26,34 @@ class ArticleAdapter(val items : JSONArray, val context: Context) : RecyclerView
         return ViewHolder(LayoutInflater.from(context).inflate(R.layout.recycler_article_list_item, parent, false))
     }
 
-    // Binds each article title in the ArrayList to a view
+    // Binds each article in the ArrayList to a view
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.tvTitle.text = items.getJSONObject(position).getString("title")
+        val article = items.getJSONObject(position)
+
+        Picasso.get()
+            .load(prefs.getString("server","<unset>") + article.getString("imageURL"))
+            .resize(50, 50)
+            .centerCrop()
+            .into(holder.ivImage)
+
+        holder.tvTitle.text = article.getString("title")
+
+        val excerpt = article.getString("excerpt")
+        holder.tvExcerpt.text = if (excerpt.length > 150) excerpt.take(150) + "..." else excerpt
+
+        val tagsArray = article.getJSONArray("tags")
+        val tags = ArrayList<String>()
+        for (i in 0 until tagsArray.length()) {
+            tags.add(tagsArray.getJSONObject(i).getString("name"))
+        }
+        holder.tvTags.text = tags.joinToString()
     }
 }
 
 class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     // Holds the TextView that will add each article to
-    val tvTitle: TextView = view.tv_article_title
+    val ivImage: ImageView = view.row_image
+    val tvTitle: TextView = view.row_title
+    val tvExcerpt: TextView = view.row_excerpt
+    val tvTags: TextView = view.row_tags
 }
